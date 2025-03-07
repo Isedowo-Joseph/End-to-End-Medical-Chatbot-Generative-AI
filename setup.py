99f9e0a932535538653d09827e79bf7237adf237
JosephIsedowo <61061657+JosephIsedowo@users.noreply.github.com>
2025-03-07 13:57:08 -0500

    folder structure added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(
+    name = 'Generative AI Project',
+    version = '0.0.0',
+    author = 'Joseph Isedowo',
+    author_email = 'joseph.isedowo12@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
